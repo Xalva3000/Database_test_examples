e5ab05d075e024f922d5bc213eb0e16ceccd5128
Alexandr Milto <maasania@gmail.com>
2024-07-20 18:49:32 +0400

    concepts red

--- concept/book_concept.py
+++ concept/book_concept.py
@@ -5,12 +5,11 @@ print(BookID)
 
 
 class BookConcept(BookID):
-    def __init__(self, title, author, page_size=1050):
+    def __init__(self, title, page_size=1050):
         self.title = title
-        self.author = author
         self.page_size = page_size
         self.stories = []
-        self.book: dict[int, str] = {}
+        self.pages: dict[int, str] = {}
         self._file_path = None
 
 
@@ -26,3 +25,25 @@ class BookConcept(BookID):
     def file_path(self):
         self._file_path = None
 
+    def _get_part_text(self, text, start, page_size):
+        char_end = ',.!:;?'
+        part = text[start:start + page_size]
+        for i_b in range(-1, -len(part) - 1, -1):
+            if part[i_b] in char_end:
+                end = (start + page_size + i_b + 1) if (start + page_size + i_b + 1) <= len(text) else len(text)
+                result = text[start:end]
+                if result[-1] in char_end and result[-2] in char_end and result[-3] not in char_end:
+                    result = self._get_part_text(text, start, page_size - 2)[0]
+                return result, len(result)
+
+    def prepare_book(self) -> None:
+        indent = 0
+        count = 1
+        while indent < len(self.text):
+            tpl = self._get_part_text(self.text, indent, self.page_size)
+            if tpl[0]:
+                self.pages[count] = tpl[0].lstrip()
+                indent += tpl[1]
+                count += 1
+            else:
+                break
